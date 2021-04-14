5cd7d48246678ebefb78da9bd4eb219a415dab53
Maeva Tellier-Simenel <74654894+MaevaTellierSimenel@users.noreply.github.com>
2021-04-14 11:35:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,6 +13,15 @@ int main(){
     Monstre monMonstre(10, 50, "Carl");
     Monstre monstreEnnemi(5, 20, "Bruh");
 
+    Mage myself ("Random", 10000);
+    Mage ennemy ("Simp", 50);
+
+    myself.getNewMonster();
+    ennemy.getNewMonster();
+    myself.attackChoice();
+    ennemy.attackchoice();
+    
+
     while(monMonstre.testdead()== false && monstreEnnemi.testdead()== false){
         monMonstre.attack(attackUs);
         monstreEnnemi.attack(attackEnnemy);
