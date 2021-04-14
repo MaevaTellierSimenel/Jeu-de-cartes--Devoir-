6926c112006c7e6c29201e696044b2370cbdd7c6
Maeva Tellier-Simenel <74654894+MaevaTellierSimenel@users.noreply.github.com>
2021-04-14 09:53:07 +0200

    Update Monstre.cpp

--- Monstre.cpp
+++ Monstre.cpp
@@ -3,22 +3,30 @@
 #include <iostream>
 #include "Monstre.h"
 
-void Monstre::attack(int _attackscore, bool _exhausted, int attackUs){
-    if ((_exhausted) == true){
+void Monstre::attack(int _attackscore, bool _exhausted, int attackUs, bool _dead){
+    if ((_exhausted) ==  false && (_dead) == false){
         attackUs = (_attackscore);
         std::cout<<"Votre monstre inflige "<<attackUs<<" dégâts au monstre adverse."<<std::endl;
+        (_exhausted) = true;
+    }
+    else if ((_exhausted) == true) {
+        std::cout<<"Votre monstre est fatigué, il ne peux plus être utiliser."<<std::endl;
     }
     else {
-        std::cout<<"Votre monstre est fatigué, il ne peux plus être utiliser."<<std::endl;
+        std::cout<<"Votre monstre est mort, il ne peux plus être utiliser."<<std::endl;
     }
 }
 
 void Monstre::damagetook(int _lifepoints, int attackEnnemy){
-
+    (_lifepoints) = (_lifepoints)-attackEnnemy;
+    std::cout<<"L'adversaire a infligé "<<attackEnnemy<<" de dégâts à votre monstre, il lui reste "<<(_lifepoints)<<" points de vie."<<std::endl;
 }
 
-void Monstre::testdead(int _lifepoints, bool dead){
-
+void Monstre::testdead(int _lifepoints, bool _dead){
+    if ((_lifepoints) <= 0){
+        (_dead) = true;
+        std::cout<<"Votre monstre est mort."<<std::endl;
+    }
 }
 
 void Monstre::printmonster(int attackscore, int lifepoints, bool exhausted, bool dead, std::string name){
