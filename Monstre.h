5bb05f1ecf760e8e4d334cdf34216c6dc4bbc03c
Maeva Tellier-Simenel <74654894+MaevaTellierSimenel@users.noreply.github.com>
2021-04-14 11:24:56 +0200

    Update Monstre.h

--- Monstre.h
+++ Monstre.h
@@ -2,10 +2,14 @@
 #include <string>
 #include "Mage.h"
 
-class Monstre : virtual Mage {
+class Monstre{
     private :
+
         int _attackscore;
+        int _lifepoints;
         bool _exhausted = false;
+        bool _dead = false;
+        std::string _name;
 
     public :
         void attack(int attackUs);
