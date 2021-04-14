5ae2ec040e745b9591b688ec6ca7e84bd8a63ac8
Maeva Tellier-Simenel <74654894+MaevaTellierSimenel@users.noreply.github.com>
2021-04-14 11:07:06 +0200

    Update Mage.cpp

--- Mage.cpp
+++ Mage.cpp
@@ -2,6 +2,32 @@
 #include <string>
 #include <iostream>
 
-void getNewMonster();
-void attackChoice();
-void mageTookDamages();
+bool getNewMonster(){
+    bool i = false;
+    std::string answer;
+    while (i == false){
+        std::cout<<"Voulez-vous avoir un nouveau monstre ? Yes or No"<<std::endl;
+        std::cin>>answer;
+        if (answer == "Yes"){
+            i == true;
+            return true;
+        }
+        else if (answer == "No"){
+            i == true;
+            return false;
+        }
+        else {
+            i = false;
+        }
+    }
+    
+
+}
+
+void attackChoice(){
+
+}
+
+void mageTookDamages(){
+
+}
