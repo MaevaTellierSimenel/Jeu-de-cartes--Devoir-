9940d816736adc2e9c2d9a7550971f37c6405f9d
Maeva Tellier-Simenel <74654894+MaevaTellierSimenel@users.noreply.github.com>
2021-04-14 09:53:05 +0200

    Update Monstre.h

--- Monstre.h
+++ Monstre.h
@@ -5,12 +5,12 @@ class Monstre {
     private :
         int _attackscore;
         int _lifepoints;
-        bool _exhausted;
-        bool _dead;
+        bool _exhausted = false;
+        bool _dead = false;
         std::string _name;
 
     public :
-        void attack(int attackscore, bool exhausted, int attackUs);
+        void attack(int attackscore, bool exhausted, int attackUs, bool dead);
         void damagetook(int lifepoints, int attackscoreAutre);
         void testdead(int lifepoints, bool dead);
         void printmonster(int attackscore, int lifepoints, bool exhausted, bool dead, std::string name);
