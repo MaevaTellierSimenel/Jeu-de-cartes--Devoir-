098ff6d9dbf6c142b68c5913fb668d4ed967dbb4
Maeva Tellier-Simenel <74654894+MaevaTellierSimenel@users.noreply.github.com>
2021-04-14 10:28:13 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,20 @@ using namespace std;
 
 int main(){
 
-    Monstre monMonstre(10, 50, "Carl");
+    int attackUs;
+    int attackEnnemy;
 
+    Monstre monMonstre(10, 50, "Carl");
+    Monstre monstreEnnemi(5, 20, "Bruh");
+
+    while(monMonstre.testdead()== false && monstreEnnemi.testdead()== false){
+        monMonstre.attack(attackUs);
+        monstreEnnemi.attack(attackEnnemy);
+        monMonstre.damagetook(attackEnnemy);
+        monstreEnnemi.damagetook(attackUs);
+        monMonstre.testdead();
+        monstreEnnemi.testdead();
+        monMonstre.printmonster();
+        monstreEnnemi.printmonster();
+    }
 }
