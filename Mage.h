4b9545219fbcdf440ce40ea1168a5698e5be1aa5
Maeva Tellier-Simenel <74654894+MaevaTellierSimenel@users.noreply.github.com>
2021-04-14 11:24:59 +0200

    Update Mage.h

--- Mage.h
+++ Mage.h
@@ -4,15 +4,14 @@
 class Mage{
 
     private :
-        std::string _name;
-        int _lifepoints;
-        bool _dead;
-        std::string _arrayofmonsters[];
+        std::string _magename;
+        int _magelifepoints;
+        bool _magedead;
+        float _mageChoice;
+        std::string _magearrayofmonsters[];
     
     public :
-        void getNewMonster();
-        void attackChoice();
-
-        
-
+        bool getNewMonster();
+        bool attackChoice();
+        void mageTookDamages();
 };
